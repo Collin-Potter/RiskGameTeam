3c3cf7f0c10bc99fd950c227fef393bb03441ef2
Grant Williams <grantwilliams7@yahoo.com>
2018-10-16 15:01:39 -0500

    Update TwitterHandler.java

--- COSC4353RiskGame/src/main/java/twitter/TwitterHandler.java
+++ COSC4353RiskGame/src/main/java/twitter/TwitterHandler.java
@@ -1,7 +1,12 @@
+package twitter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
+import main.Helper;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -23,25 +28,38 @@ public class TwitterHandler {
 	private static String accessTokenSecretStr = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 	private Twitter twitterHandler;
 	private Scanner userInput;
+	private Helper helper;
+	private BufferedReader br;
+	private FileReader fr;
 
 	public TwitterHandler(){
 		twitterHandler = new TwitterFactory().getInstance();
+		helper = new Helper();
 		String in = "";
 		userInput = new Scanner(System.in);
 		
+		/*
 		//Get private Twitter info from user
 		System.out.println("Enter Consumer Key for Twitter API: ");
 		in = userInput.nextLine();
 		consumerKeyStr = in;
+		System.out.println(helper.daBar);
 		System.out.println("Enter Consumer Secret Key for Twitter API: ");
 		in = userInput.nextLine();
 		consumerSecretStr = in;
+		System.out.println(helper.daBar);
 		System.out.println("Enter Access Token for Twitter API: ");
 		in = userInput.nextLine();
 		accessTokenStr = in;
+		System.out.println(helper.daBar);
 		System.out.println("Enter Access Token Secret for Twitter API: ");
 		in = userInput.nextLine();
 		accessTokenSecretStr = in;
+		System.out.println(helper.daBar);
+		*/
+		
+		//Get Twitter credentials
+		readInCredentials("C:\\Users\\grant\\git\\RiskGameGrantBranch\\COSC4353RiskGame\\src\\main\\java\\twitter\\twitterCredentials");
 		
 		//Authenticate user
 		twitterHandler.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
@@ -60,11 +78,9 @@ public class TwitterHandler {
 	
 	public void deleteTweets(){
 		try {
-			while(!twitterHandler.getUserTimeline().isEmpty()){
-				List<Status> status = twitterHandler.getUserTimeline();
-				for(Status st: status){
-					twitterHandler.destroyStatus(st.getId());
-				}
+			List<Status> status = twitterHandler.getUserTimeline();
+			for(Status st: status){
+				twitterHandler.destroyStatus(st.getId());
 			}
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
@@ -72,6 +88,43 @@ public class TwitterHandler {
 		} catch (TwitterException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	private void readInCredentials(String filename){
+		try{
+			fr = new FileReader(filename);
+			br = new BufferedReader(fr);
+			String line = null;
+			int count = 0;
+			while((line = br.readLine()) != null){
+				if(count == 0){
+					consumerKeyStr = line;
+				}
+				if(count == 1){
+					consumerSecretStr = line;
+				}
+				if(count == 2){
+					accessTokenStr = line;
+				}
+				if(count == 3){
+					accessTokenSecretStr = line;
+				}
+				count++;
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(br != null){
+					br.close();
+				}
+				if(fr != null){
+					fr.close();
+				}
+			}catch(IOException ex){
+				ex.printStackTrace();
+			}
 		}
 	}
 }
