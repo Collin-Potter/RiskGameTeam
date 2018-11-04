f8a42e5aeebf642cd12969121807c9035bf455c7
Grant Williams <grantwilliams7@yahoo.com>
2018-11-04 08:26:54 -0600

    Create TelegramBot.java
    TelegramBot class to be used by TelegramBotHandler class

---
+++ src/main/java/BaseGameEssentials/TelegramBot.java
@@ -0,0 +1,86 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+//This class creates the instance of the bot to be manioulated by the bot handler class
+public class TelegramBot extends TelegramLongPollingBot {
+	
+	private static String botKeyStr = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	private BufferedReader br;
+	private FileReader fr;
+	
+	public TelegramBot(){
+		
+		//Get TelegramBot credentials
+        readInCredentials("/src/main/java/BaseGameEssentials/telegramBotToken");
+	}
+	
+    @Override
+    public void onUpdateReceived(Update update) {
+    	// We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            
+            //Determines content of message
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "CSGRiskGame_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return botKeyStr;
+    }
+    
+    //Read in BotToken
+    private void readInCredentials(String filepath){
+		try{
+			File currentDir = new File(".");
+	        File parentDir = currentDir.getAbsoluteFile();
+	        File newFile = new File(parentDir + filepath);
+	        fr = new FileReader(newFile);
+	        br = new BufferedReader(fr);
+			String line = null;
+			int count = 0;
+			while((line = br.readLine()) != null){
+				if(count == 0){
+					botKeyStr = line;
+				}
+				count++;
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(br != null){
+					br.close();
+				}
+				if(fr != null){
+					fr.close();
+				}
+			}catch(IOException ex){
+				ex.printStackTrace();
+			}
+		}
+	}
+}
